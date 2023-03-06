e6f5f89023d3d850e519a9f87ae628df73430e63
sebasbocanegrab <sbocanegra17@misena.edu.co>
2023-03-06 18:36:12 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log(data)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          
+          data.results.map(item => 
+            <div key={item.name}> {item.name}</div>
+            )
+            
+        }
+        Hola mundo
       </header>
     </div>
   );
